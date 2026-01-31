b3bc4786ba94306dcfd4a81b38b5dc3367b0e147
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2026-01-31 15:19:34 +1300

    fix: Backend listen to all

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,6 @@ import "dotenv/config";
 import { app } from "@/app.js";
 import { config, logger } from "@/config/index.js";
 
-app.listen(config.port, () => {
+app.listen(config.port, "0.0.0.0", () => {
   logger.info({ port: config.port }, "Server started");
 });
